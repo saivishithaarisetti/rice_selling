7149131ffeab31d0f05814e185258e5d5a87ff57
saivishitharisetti <saivishithaarisetti@gmail.com>
2025-03-14 22:30:49 +0530

    path update

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Header />
           <main>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/rice_selling/" element={<Home />} />
               <Route path="/products" element={<Products />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/about" element={<About />} />
