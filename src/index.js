1451c9e71e8dab0082f6bf7d0a6c204de1d2d13b
saivishitharisetti <saivishithaarisetti@gmail.com>
2025-03-25 15:58:58 +0530

    changing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie11';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
